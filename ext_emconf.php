c2d5ac3877a9806ba03e5348896ad0d706b7a8d5
mehrwert <mehrwert@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-07-15 09:33:41 +0000

    [TASK] Create tag for phpMyAdmin 4.19.1
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/phpmyadmin/tags/phpmyadmin_4_19_1@89477 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,50 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "phpmyadmin".
+#
+# Auto generated 21-05-2011 12:02
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'phpMyAdmin',
+	'description' => 'Third party \'phpMyAdmin\' administration module. Access to admin-users only. 4.x releases require PHP 5.2, TYPO3 4.2 and MySQL 5. The 3.x branch is still supported: http://www.mehrwert.de/go/t3x',
+	'category' => 'module',
+	'shy' => 0,
+	'version' => '4.19.1',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => 'modsub',
+	'state' => 'stable',
+	'uploadfolder' => 1,
+	'createDirs' => 'uploads/tx_phpmyadmin',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Andreas Beutel - mehrwert',
+	'author_email' => 'typo3@mehrwert.de',
+	'author_company' => 'mehrwert intermediale kommunikation GmbH',
+	'doNotLoadInFE' => 1,
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '5.2.0-5.4.0',
+			'typo3' => '4.2.0-6.2.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	)
+);
+
+?>
